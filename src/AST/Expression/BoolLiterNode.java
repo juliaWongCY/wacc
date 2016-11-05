b69fd22bde06495c988b5b67d7b10fc2c43195dd
hlf15 <dexterfung0816@gmail.com>
2016-11-05 02:21:46 +0000

    [DF] Started basicLiter Nodes implementation (VERY BASIC)

--- src/AST/Expression/BoolLiterNode.java
+++ src/AST/Expression/BoolLiterNode.java
@@ -1,4 +1,15 @@
 package AST.Expression;
 
 public class BoolLiterNode implements ExpressionNode {
+
+    private final boolean value;
+
+    public BoolLiterNode(boolean value) {
+        this.value = value;
+    }
+
+    public boolean getValue() {
+        return value;
+    }
+
 }
--- src/AST/Expression/IntLiterNode.java
+++ src/AST/Expression/IntLiterNode.java
@@ -1,4 +1,15 @@
 package AST.Expression;
 
 public class IntLiterNode implements ExpressionNode {
+
+    private final int value;
+
+    public IntLiterNode(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
 }

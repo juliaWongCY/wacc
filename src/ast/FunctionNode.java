bbf8076b6f41529942284b3f42af73a7207eb0b0
Julia Wong <ching.wong15@imperial.ac.uk>
2016-11-05 17:55:04 +0000

    [JW] Implemented some functions in FunctionNode

--- src/ast/FunctionNode.java
+++ src/ast/FunctionNode.java
@@ -1,11 +1,39 @@
 package ast;
 
+import ast.parameter.ParamListNode;
+import ast.statement.StatementNode;
 import org.antlr.runtime.UnwantedTokenException;
 import org.antlr.v4.runtime.ParserRuleContext;
+import type.Type;
 
 public class FunctionNode implements ASTNode {
 
     //<func> ::= <type> <ident> ‘(’ <param-list>? ‘)’ ‘is’ <stat> ‘end’
-    
+
+    private Type type;
+    private IdentNode ident;
+    private ParamListNode paramList;
+    private StatementNode stat;
+
+
+    public Type getType(){
+        return type;
+    }
+
+    public IdentNode getIdentNode(){
+        return ident;
+    }
+
+    public ParamListNode getParamListNode(){
+        return paramList;
+    }
+
+    public StatementNode getStatement(){
+        return stat;
+    }
+
+
+
+
 
 }

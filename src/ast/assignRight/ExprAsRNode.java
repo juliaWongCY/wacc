f3d360b0026921fa91956f77f03e3a2f558fc36a
Dexter_Laptop_hlf15 <hlf15@ic.ac.uk>
2016-11-07 23:02:39 +0000

    [DF] Added assignRight - ExprAsRNode

--- src/ast/assignRight/ExprAsRNode.java
+++ src/ast/assignRight/ExprAsRNode.java
@@ -1,4 +1,25 @@
 package ast.assignRight;
 
+import ast.expression.ExpressionNode;
+import frontEnd.SemanticException;
+import frontEnd.SymbolTable;
+import type.Type;
+
 public class ExprAsRNode implements AssignRightNode {
+
+    private ExpressionNode expressionNode;
+
+    public ExprAsRNode(ExpressionNode expressionNode) {
+        this.expressionNode = expressionNode;
+    }
+
+    @Override
+    public Type getNodeType(SymbolTable st) throws SemanticException {
+        try {
+            return expressionNode.getNodeType(st);
+        } catch (SemanticException e) {
+            throw new SemanticException("ERROR");
+        }
+    }
+
 }

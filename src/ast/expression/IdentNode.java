d779024ba55480accc631e7b00214a9f1f6f7cc0
hlf15 <dexterfung0816@gmail.com>
2016-11-07 11:29:08 +0000

    [DF] Updated ASTNode and Working on IdentNode

--- src/ast/expression/IdentNode.java
+++ src/ast/expression/IdentNode.java
@@ -4,8 +4,20 @@ import ast.ASTNode;
 import type.Type;
 
 public class IdentNode implements ExpressionNode {
+
+    private final String id;
+
+    public Ident(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+
     @Override
     public Type getNodeType() {
-        return null;
+
     }
 }

f46c823aa4811b78baa7a43fd59c56a432419b2f
Dexter_Laptop_hlf15 <hlf15@ic.ac.uk>
2016-11-08 11:46:16 +0000

    [DF] Working on PairType and PairElemType so that NewPairAsRNode can be done.

--- src/ast/assignRight/NewPairAsRNode.java
+++ src/ast/assignRight/NewPairAsRNode.java
@@ -1,12 +1,28 @@
 package ast.assignRight;
 
+import ast.expression.ExpressionNode;
 import frontEnd.SemanticException;
 import frontEnd.SymbolTable;
+import type.PairElemType;
+import type.PairType;
 import type.Type;
 
 public class NewPairAsRNode implements AssignRightNode {
+
+
+    //Not sure if it is PairElemType or ExpressionNode
+    //It should be ExpressionNode since NewPairAsRNode initialises a pair
+    private ExpressionNode fstElem;
+    private ExpressionNode sndElem;
+
+    public NewPairAsRNode(PairElemType fstElem, PairElemType sndElem) {
+        this.fstElem = fstElem;
+        this.sndElem = sndElem;
+    }
+
     @Override
     public Type getNodeType(SymbolTable st) throws SemanticException {
-        return null;
+        PairType pairType = new PairType(fstElem, sndElem);
+        return pairType.getType();
     }
 }
